b75541e206f21b4415b4542d51b9fdad839311b1
Matcham89 <cjmatcham@hotmail.co.uk>
2026-03-26 15:28:01 -0600

    define colour scheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "tiesen243/vercel.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      theme = "dark",
+      transparent = false,
+    },
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "vercel",
+    },
+  },
+}
