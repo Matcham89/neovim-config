e567c6300904e7151b613eaa8faf658c90173e33
cmatcham <cmatcham@helcim.com>
2026-04-07 09:54:09 -0600

    set them to catppuccin

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,30 +1,25 @@
 return {
   {
-    "folke/tokyonight.nvim",
+    "catppuccin/nvim",
+    name = "catppuccin",
     lazy = false,
     priority = 1000,
     opts = {
-      style = "day",
-      day_brightness = 0.45,
-      on_highlights = function(hl, c)
-        hl.Keyword = { fg = c.orange, bold = true }
-        hl.String = { fg = c.green1 }
-        hl.Type = { fg = c.blue, bold = true }
-        hl["@keyword"] = { fg = c.orange, bold = true }
-        hl["@string"] = { fg = c.green1 }
-        hl["@type"] = { fg = c.blue, bold = true }
-        hl["@type.builtin"] = { fg = c.magenta }
-        hl["@property"] = { fg = c.blue5, bold = true }
-        hl["@variable.member"] = { fg = c.blue5 }
-        hl["@punctuation.special"] = { fg = c.magenta }
-      end,
+      flavour = "latte",
+      background = {
+        light = "latte",
+        dark = "mocha",
+      },
+      transparent_background = false,
+      term_colors = false,
+      no_italic = true,
     },
   },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight-day",
+      colorscheme = "catppuccin-latte",
     },
   },
 }
