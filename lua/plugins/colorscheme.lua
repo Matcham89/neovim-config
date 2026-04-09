61f0b38a6cc2e6322cb0ab0decb150a4143327db
cmatcham <cmatcham@helcim.com>
2026-04-09 10:24:43 -0600

    disable noice

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -22,4 +22,22 @@ return {
       colorscheme = "catppuccin-mocha",
     },
   },
+
+  {
+    "folke/snacks.nvim",
+    opts = function(_, opts)
+      local now = os.date("*t")
+      local hour = now.hour % 12
+      if hour == 0 then
+        hour = 12
+      end
+
+      opts.dashboard = opts.dashboard or {}
+      opts.dashboard.preset = opts.dashboard.preset or {}
+      opts.dashboard.preset.header = table.concat({
+        os.date("%A"),
+        string.format("%d:%02d %s", hour, now.min, now.hour >= 12 and "PM" or "AM"),
+      }, "\n")
+    end,
+  },
 }
