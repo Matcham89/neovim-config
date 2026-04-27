a32d40e34ae9027787cdf520a3d57874e04e487e
matcham89 <cjmatcham@hotmail.co.uk>
2026-04-27 12:30:41 -0400

    feat: show hidden files in snacks picker

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -3,4 +3,16 @@ return {
     "folke/noice.nvim",
     enabled = false,
   },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        sources = {
+          files = { hidden = true },
+          grep = { hidden = true },
+          explorer = { hidden = true },
+        },
+      },
+    },
+  },
 }
