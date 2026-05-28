32ac897611ddf0c61cca598f72c6751a15d54541
matcham89 <cjmatcham@hotmail.co.uk>
2026-05-28 17:08:09 -0400

    set theme to default

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,25 +1,8 @@
 return {
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    lazy = false,
-    priority = 1000,
-    opts = {
-      flavour = "mocha",
-      background = {
-        light = "latte",
-        dark = "mocha",
-      },
-      transparent_background = false,
-      term_colors = false,
-      no_italic = true,
-    },
-  },
-
-  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-mocha",
+      colorscheme = "default",
     },
   },
 
